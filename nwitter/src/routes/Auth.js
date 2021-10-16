4349a4e699c4feac69ff9050aa3f1071eb043ccf
LeeJia <abell2667@gm.gist.ac.kr>
2021-10-16 19:51:35 +0900

    2.4 Log in

--- nwitter/src/routes/Auth.js
+++ nwitter/src/routes/Auth.js
@@ -1,19 +1,39 @@
+import { authService } from "fbase";
 import React, { useState } from "react";
+import { createUserWithEmailAndPassword, signInWithPopup,signInWithEmailAndPassword } from "firebase/auth";
 
 const Auth = () => {
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
+    const [newAccount, setNewAccount] = useState(true);
+    const [error, setError] = useState("");
     const onChange = (event) => {
-        const {target: {name, value}} = event;
+        const {
+            target: {name, value},
+        } = event;
+
         if(name === "email"){
             setEmail(value);
-        } else if(name === "password"){
+        } else if (name === "password"){
             setPassword(value);
         }
     };
-    const onSubmit = (event) => {
+    const onSubmit = async (event) => {
         event.preventDefault();
+        try {
+            let data;
+            if (newAccount) {
+                const data = await createUserWithEmailAndPassword(authService,email,password);
+            } else {
+                const data = await signInWithEmailAndPassword(authService,email,password);
+            } 
+            console.log(data);
+        } catch (error) {
+            setError(error.message);
+        }
     };
+const toggleAccount = () => setNewAccount((prev) => !prev);
+
     return (
         <div>
             <form onSubmit={onSubmit}>
@@ -33,13 +53,17 @@ const Auth = () => {
                     value={password} 
                     onChange={onChange}
                 />
-                <input type="submit" value="Log In" />
+                <input type="submit" value={newAccount ? "Create Account" : "Log In"} />
+                {error}
             </form>
+            <span onClick={toggleAccount}>
+                {newAccount ? "Sign in" : "Create Account"}
+            </span>
             <div>
                <button>Continue with Google</button>
                <button>Continue with Github</button>
             </div>
         </div>
     );
-}
+};
 export default Auth;
