fe2b226856d2712fe15d2a58eeb8f0a6b200529c
LeeJia <abell2667@gm.gist.ac.kr>
2021-10-16 20:05:02 +0900

    2.5 Social Login

--- nwitter/src/routes/Auth.js
+++ nwitter/src/routes/Auth.js
@@ -1,6 +1,7 @@
-import { authService } from "fbase";
+import { authService, firebaseInstance } from "fbase";
 import React, { useState } from "react";
 import { createUserWithEmailAndPassword, signInWithPopup,signInWithEmailAndPassword } from "firebase/auth";
+import { GithubAuthProvider, GoogleAuthProvider } from "firebase/auth";
 
 const Auth = () => {
     const [email, setEmail] = useState("");
@@ -33,6 +34,20 @@ const Auth = () => {
         }
     };
 const toggleAccount = () => setNewAccount((prev) => !prev);
+const onSocialClick = async (event) => {
+    const 
+    {
+        target:{name},
+    } = event;
+    let provider;
+    if (name == "google") {
+        provider = new GoogleAuthProvider();
+    } else if (name == "github") {
+        provider = new GithubAuthProvider();
+    }
+    const data = await signInWithPopup(authService,provider);
+    console.log(data);
+};
 
     return (
         <div>
@@ -60,8 +75,12 @@ const toggleAccount = () => setNewAccount((prev) => !prev);
                 {newAccount ? "Sign in" : "Create Account"}
             </span>
             <div>
-               <button>Continue with Google</button>
-               <button>Continue with Github</button>
+               <button onClick={onSocialClick} name="google">
+                   Continue with Google
+                </button>
+               <button onClick={onSocialClick} name="github">
+                   Continue with Github
+                </button>
             </div>
         </div>
     );
