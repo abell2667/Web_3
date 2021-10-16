902c49eab5a25988947424bbd5ba580bf638d63b
LeeJia <abell2667@gm.gist.ac.kr>
2021-10-17 02:03:14 +0900

    3.1 Nweeting!

--- nwitter/src/routes/Home.js
+++ nwitter/src/routes/Home.js
@@ -1,9 +1,21 @@
+import { dbService } from "fbase";
 import React, { useState } from "react";
+import { addDoc, collection } from "firebase/firestore";
 
 const Home = () => {
     const [nweet, setNweet] = useState("");
-    const onSubmit = (event) => {
+    const onSubmit = async (event) => {
         event.preventDefault();
+        try {
+            const docRef = await addDoc(collection(dbService, "nweets"), {
+            nweet,
+            createdAt: Date.now(),
+            });
+            console.log("Document written with ID: ", docRef.id);
+        } catch (error) {
+        console.error("Error adding document: ", error);
+        }
+        setNweet("");
     };
     const onChange = (event) => {
         const { 
