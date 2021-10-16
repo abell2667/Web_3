c4dfa1a5c6452d495cd63d3e6709e94b14f5a49a
LeeJia <abell2667@gm.gist.ac.kr>
2021-10-17 02:27:45 +0900

    3.2 Getting the Nweets

--- nwitter/src/routes/Home.js
+++ nwitter/src/routes/Home.js
@@ -1,9 +1,23 @@
 import { dbService } from "fbase";
-import React, { useState } from "react";
-import { addDoc, collection } from "firebase/firestore";
+import React, { useEffect, useState } from "react";
+import { collection, addDoc, getDocs } from "firebase/firestore"
 
 const Home = () => {
     const [nweet, setNweet] = useState("");
+    const [nweets, setNweets] = useState([]);
+    const getNweets = async () => {
+        const dbNweets = await getDocs(collection(dbService, "nweets"));
+        dbNweets.forEach((document) => {
+            const nweetObject = {
+                ...document.data(),
+                id: document.id,
+            };
+            setNweets((prev) => [nweetObject, ...prev]);
+        });
+    };
+    useEffect(() => {    
+        getNweets();
+    }, []);
     const onSubmit = async (event) => {
         event.preventDefault();
         try {
@@ -23,6 +37,7 @@ const Home = () => {
         } = event;
         setNweet(value);
     };
+    console.log(nweets);
     return (
         <div>
             <form onSubmit={onSubmit}>
@@ -38,7 +53,13 @@ const Home = () => {
                     value="Nweet" 
                 />
             </form>
-        </div>       
+            <div>
+                {nweets.map((nweet) => 
+                <div key={nweet.id}>
+                    <h4>{nweet.nweet}</h4>
+                </div>)}    
+            </div> 
+        </div>  
     );
 };
 export default Home;
