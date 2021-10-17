3a5a34c9d85cb96fab3007882231bd495f6a12bc
LeeJia <abell2667@gm.gist.ac.kr>
2021-10-17 14:11:27 +0900

    3.4 Delete and Update part One

--- nwitter/src/routes/Home.js
+++ nwitter/src/routes/Home.js
@@ -1,6 +1,7 @@
 import { dbService } from "fbase";
 import React, { useEffect, useState } from "react";
 import { collection, addDoc, getDocs, onSnapshot, orderBy, query  } from "firebase/firestore"
+import Nweet from "components/Nweet";
 
 const Home = ({ userObj }) => {
     console.log(userObj);
@@ -53,10 +54,13 @@ const Home = ({ userObj }) => {
                 />
             </form>
             <div>
-                {nweets.map((nweet) => 
-                <div key={nweet.id}>
-                    <h4>{nweet.text}</h4>
-                </div>)}    
+                {nweets.map((nweet) => (
+                     <Nweet
+                     key={nweet.id}
+                     nweetObj={nweet}
+                     isOwner={nweet.creatorId === userObj.uid}
+                   />
+                ))}  
             </div> 
         </div>  
     );
