bf86ae5a323077c45fc0463e7f2cb936ce4543a9
LeeJia <abell2667@gm.gist.ac.kr>
2021-10-17 18:11:00 +0900

    4.1 Preview Images part Two

--- nwitter/src/routes/Home.js
+++ nwitter/src/routes/Home.js
@@ -7,6 +7,7 @@ const Home = ({ userObj }) => {
     console.log(userObj);
     const [nweet, setNweet] = useState("");
     const [nweets, setNweets] = useState([]);
+    const [attachment, setAttachment] = useState();
     useEffect( () => {
         onSnapshot (
         query(collection(dbService, "nweets"), orderBy("createdAt", "desc")),
@@ -45,10 +46,14 @@ const Home = ({ userObj }) => {
         const theFile = files[0];
         const reader = new FileReader();
         reader.onloadend = (finishedEvent) => {
-            console.log(finishedEvent);
+            const {
+                currentTarget: { result },
+            } = finishedEvent;
+            setAttachment(result);
         };
         reader.readAsDataURL(theFile);
     };
+    const onClearAttachment = () => setAttachment(null); 
     return (
         <div>
             <form onSubmit={onSubmit}>
@@ -64,6 +69,12 @@ const Home = ({ userObj }) => {
                     type="submit" 
                     value="Nweet" 
                 />
+                {attachment && (
+                    <div>
+                        <img src={attachment} width="50px" height="50px" />
+                        <button onClick={onClearAttachment}>Clear</button>
+                    </div>
+                )}
             </form>
             <div>
                 {nweets.map((nweet) => (
